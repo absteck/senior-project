f12e6076d9feab0258c4b54662dc1a52dca3626b
absteck <65784718+absteck@users.noreply.github.com>
2021-11-09 16:19:59 -0500

    creates separate plots for full vs base models
    Copied from KLM's plots.R. Produces similar output to KLM Figure 4

---
+++ plot_script.R
@@ -0,0 +1,492 @@
+## data manipulation
+library(plyr)
+library(reshape2)
+
+## output
+library(ggplot2)
+library(xtable)
+
+##########
+## prep ##
+##########
+
+red <- '#A31F34'
+blue <- '#325585'
+
+odds.to.mean <- function(x){ x / (x + 1) }
+mean.to.odds <- function(x){ x / (1 - x) }
+
+#packages for loading in compressed data 
+library(R.utils)
+library(tidyverse)
+combine <- read.csv('/Users/abbysteckel/Desktop/combined.csv.gz')
+combine <- na_if(combine, "")
+
+#subset to just Black or white folks 
+df <- combine[combine$RACE %in% c("black", "white"), ]
+df$RACE <- ifelse(df$RACE == "black", 1, 0)
+
+## set white as base level
+races.abbr <- c('white', 'black')
+races <- c('white', 'black')
+df$race <- factor(df$RACE, labels = races)
+
+#only 2001 data 
+df <- df[df$YR2001 == 1,]
+
+outcome <- c('SENTENCE')
+predictors <- c('AGE', 'MALE', 'HSGED', 'SOMEPOSTHS', 'POSTHSDEGREE', 'HISPANIC', 'USCITIZEN', 'CRIMINAL', 'CATEGORY2', 'CATEGORY3', 'CATEGORY4', 'CATEGORY5', 'CATEGORY6', 'NOCOUNTS', 'POINTS', 'TRIAL', 'PRIM_OFFENSE', 'race')
+
+df <- na.omit(df[, c(outcome, predictors)])
+
+#create sentence levels  
+sentence.quartiles <- c(
+  'above third quartile', 
+  'above median', 
+  'above first quartile'
+)
+
+#add a categorical sentence levels variable to the df dataframe 
+df <- df %>% mutate ( # https://dplyr.tidyverse.org/reference/mutate.html
+  sentence.quartiles = case_when( # https://dplyr.tidyverse.org/reference/case_when.html 
+    SENTENCE > 72  ~ 'above third quartile', 
+    SENTENCE > 37  ~ 'above median',
+    SENTENCE > 15 ~ 'above first quartile'
+  ))
+
+thresholds <- c(72, 37, 15)
+
+
+##################
+## load results ##
+##################
+
+## bounds results
+ndraws <- 500
+alpha <- .95 #desired significance level 
+results.fname <- sprintf('bounds_results_n%s_alpha%s_blackwhite_sentencing_full.rds', 
+                         ndraws,
+                         alpha * 100
+)
+setwd("/Users/abbysteckel/Desktop/S&DS_491/KLM_sentencing_analysis/")
+results <- readRDS(results.fname) 
+#add column containing the number of black people in the dataset 
+results$n.minority <- sum(df$race == 'black', na.rm = TRUE) 
+
+
+
+####################################################################
+## plausible values for proportion of racial stops                ##
+## based on gelman fagan kiss (2007), analysis of nypd s&f policy ##
+## and goel rao shroff(2016), precinct or prejudice               ##
+####################################################################
+
+## gfk table 1: racial differences in stop rates that cannot explained by
+##   - crime type (violent / weapons / property / drug)
+##   - racial group's criminality (as previous year's dcjs arrest rates)
+##   - precincts
+gfk17 <- read.csv('/Users/abbysteckel/Desktop/S&DS_491/KLM Replication Materials/GelmanFaganKissTable1.csv')
+gfk17 <- dcast(melt(gfk17, variable.name = 'crime.type'), ... ~ parameter)
+
+## gfk p820: convert to stops per prev year's arrest, by precinct & crime type
+##   "rates of stops (compared with previous year’s arrests)
+##    for each ethnic group, e^(µ+α_e)"
+gfk17$whites.stops.per.arrest <- exp(gfk17$intercept + gfk17$whites)
+gfk17$blacks.stops.per.arrest <- exp(gfk17$intercept + gfk17$blacks)
+
+## gfk proportion of excess stops, by precinct & crime type
+gfk17$blacks.stops.excess <-
+  (gfk17$blacks.stops.per.arrest - gfk17$whites.stops.per.arrest) /
+  gfk17$blacks.stops.per.arrest
+
+## or simply do this:
+all.equal(gfk17$blacks.stops.excess,
+          1 - exp(gfk17$whites - gfk17$blacks),
+          tolerance = 1e-6
+)
+
+## gfk p817: "each of the three [black population proportion] categories
+##            represents roughly 1/3 of the precincts"
+prop.weights <- c(
+  '<10%' = 1/3,
+  '10-40%' = 1/3,
+  '>40%' = 1/3
+)
+gfk17$prop.weight <- prop.weights[gfk17$prop.black]
+
+## gfk figure 2: suspected crime type as percentage of all stops
+crime.type.weights <- c(
+  violent = .25,
+  weapons = .44,
+  property = .2,
+  drug = .11
+)
+gfk17$crime.type.weight <- crime.type.weights[gfk17$crime.type]
+
+## gfk racial stop proportions, based on
+##   (a) minority stops per each previous year's minority arrest
+##   (b) white stops per each previous year's white arrest
+##   racially motivated stops are a-b, proportion of minority stops is (a-b)/a
+##   we take weighted average of (a-b)/a by precinct type & crime type
+black.rs.gfk <- weighted.mean(
+  x = gfk17$blacks.stops.excess,
+  w = gfk17$prop.weight * gfk17$crime.type.weight
+)
+
+## grs racial stop proportions
+black.rs.grs <- (3.8 - 2.5) / 3.8
+
+## vertical lines based on prior work
+annotations <- data.frame(treat.race = 'black',
+                          rho = black.rs.gfk,
+                          text = 'GFK (2007)')
+
+
+
+#################
+## prep labels ##
+#################
+
+## labels for plots
+results$thresh.label <- factor(
+  sentence.quartiles[results$thresh],
+  levels = sentence.quartiles,
+  labels = gsub('\n', ' ', sentence.quartiles),
+  ordered = TRUE
+) #there are 200 of each threshold label: 100 rho values x 2 treatment effect estimators 
+
+results$race.label <- factor(
+  results$treat.race, #treat.race = black 
+  levels = c('black'),
+  labels = c('Black civilians'),
+  ordered = TRUE
+)
+
+results$mod.label <- factor(
+  results$mod.type,
+  levels = c('base', 'full'),
+  labels = c('Baseline specification', 'full specification'),
+  ordered = TRUE
+)
+
+annotations$race.label <- factor(
+  annotations$treat.race,
+  levels = c('black'),
+  labels = c('Black civilians'),
+  ordered = TRUE
+)
+
+
+##########################################
+## ATT_M=1 interpretation for main text ##
+##########################################
+results$rho <- round(results$rho, 2)
+
+## naive atest, baseline specification
+naive.black.base <- results[
+  results$mod.type == 'base' &
+    results$treat.race == 'black' &
+    results$rho == 0 &
+    results$qoi == 'atest',
+]
+## bias-corrected atest, baseline specification
+biascorrect.black.base <- results[
+  results$mod.type == 'base' &
+    results$treat.race == 'black' &
+    results$rho == round(black.rs.gfk, 2) &
+    results$qoi == 'atest',
+]
+
+## naive atest, full specification
+naive.black.full <- results[
+  results$mod.type == 'full' &
+    results$treat.race == 'black' &
+    results$rho == 0 &
+    results$qoi == 'atest',
+]
+## bias-corrected atest, full specification
+biascorrect.black.full <- results[
+  results$mod.type == 'full' &
+    results$treat.race == 'black' &
+    results$rho == round(black.rs.gfk, 2) &
+    results$qoi == 'atest',
+]
+
+
+  
+naive.excess <- naive.black.base$est * naive.black.base$n.minority
+
+biascorrect.excess <- biascorrect.black.base$est * biascorrect.black.base$n.minority
+
+excess.base <- cbind('naive' = naive.excess, 'bias-corrected' = biascorrect.excess)
+excess.base
+
+naive.excess <- naive.black.full$est * naive.black.full$n.minority
+
+biascorrect.excess <- biascorrect.black.full$est * biascorrect.black.full$n.minority
+excess.full <- cbind('naive' = naive.excess, 'bias-corrected' = biascorrect.excess)
+excess.full 
+
+
+##########################################
+## ATE_M=1 interpretation for main text ##
+##########################################
+
+## naive ates, baseline specification
+naive.black.base <- results[
+  results$mod.type == 'base' &
+    results$treat.race == 'black' &
+    results$rho == 0 &
+    results$qoi == 'ates',
+]
+## bias-corrected ates, baseline specification
+bounds.black.base <- results[
+  results$mod.type == 'base' &
+    results$treat.race == 'black' &
+    results$rho == round(black.rs.gfk, 2) &
+    results$qoi == 'ates',
+]
+
+## naive ates, full specification
+naive.black.full <- results[
+  results$mod.type == 'full' &
+    results$treat.race == 'black' &
+    results$rho == 0 &
+    results$qoi == 'ates',
+]
+## bias-corrected ates, full specification
+bounds.black.full <- results[
+  results$mod.type == 'full' &
+    results$treat.race == 'black' &
+    results$rho == round(black.rs.gfk, 2) &
+    results$qoi == 'ates',
+]
+
+ates.black.base <-
+  naive.black.base[,c('treat.race', 'thresh.label')]
+ates.black.base$naive <- round(1000 * naive.black.base$est, 1)
+ates.black.base$min <- round(1000 * bounds.black.base$min, 1)
+ates.black.base$max <- round(1000 * bounds.black.base$max, 1)
+ates.black.base
+
+ates.black.full <-
+  naive.black.full[,c('treat.race', 'thresh.label')]
+ates.black.full$naive <- round(1000 * naive.black.full$est, 1)
+ates.black.full$min <- round(1000 * bounds.black.full$min, 1)
+ates.black.full$max <- round(1000 * bounds.black.full$max, 1)
+ates.black.full
+
+
+#NOTE: omitted lines 435-481 because they seem the same as lines 217-228 above 
+
+
+#################
+## plot bounds ##
+#################
+
+thresh <- 1
+
+ggplot(results[results$thresh == thresh &
+                 results$qoi == 'ates',
+              ],
+      aes(x = rho)
+      ) +
+  geom_hline(yintercept = 0,
+            ) +
+  geom_ribbon(aes(ymin = min.cilo * nrow(df) / 1000,
+                  ymax = max.cihi * nrow(df) / 1000
+  ),
+  fill = paste0(red, '80')
+  ) +
+  geom_ribbon(aes(ymin = min * nrow(df) / 1000,
+                  ymax = max * nrow(df) / 1000,
+                  fill = 'TE_S'
+  )
+  ) +
+  geom_line(aes(y = est * n.minority / 1000,
+                linetype = 'TE_ST',
+  ),
+  results[results$thresh == thresh &
+            results$qoi == 'atest',
+  ],
+  color = 'black',
+  size = 1
+  ) +
+  geom_point(aes(y = est * nrow(df) / 1000,
+                 color = 'naive'
+  ),
+  data = results[results$thresh == thresh & results$rho == 0,],
+  color = blue,
+  size = 2
+  ) +
+  geom_errorbar(aes(ymin = est.cilo * nrow(df) / 1000,
+                    ymax = est.cihi * nrow(df) / 1000,
+                    color = 'naive'
+  ),
+  data = results[results$thresh == thresh & results$rho == 0,],
+  width = 1,
+  size = 1
+  ) +
+  geom_vline(aes(xintercept = rho),
+             data = annotations,
+             ## data = annotations[ifelse(rep(presentation, nrow(annotations)),
+             ##                           annotations$race == 'black',
+             ##                           TRUE
+             ##                           ),],
+             ## linetype = 'dashed',
+             size = .25
+  ) +
+  geom_text(aes(label = text),
+            data = annotations,
+            y = 1300,
+            size = 5,
+            hjust = 1,
+            vjust = -.5,
+            angle = 90
+  ) +
+  theme_light(base_size = 24) +
+  theme(legend.position = 'bottom',
+        legend.key.size = unit(1, 'cm'),
+        legend.spacing.x = unit(.1, 'cm'),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(linetype = 'dashed')
+  ) +
+  guides(color = guide_legend(title = NULL, order = 1),
+         linetype = guide_legend(title = NULL, order = 2),
+         fill = guide_legend(title = NULL, order = 3)
+  ) +
+  scale_color_manual(
+    name = 'x',
+    values = c('naive' = blue,
+               'TE_S' = NA,
+               'TE_ST' = NA
+    ),
+    labels = c('naive' = expression('naïve ATE'['M=1'] %*% ' #{convicted}'),
+               'TE_S' = expression('ATE'['M=1'] %*% ' #{convicted}'),
+               'TE_ST' = expression('ATT'['M=1'] %*% ' #{convicted minorities}')
+    )
+  ) +
+  scale_fill_manual(name = 'x',
+                    values = c('naive' = NA,
+                               'TE_S' = red,
+                               'TE_ST' = NA
+                    ),
+                    labels = c('naive' = expression('naïve ATE'['M=1'] %*% ' #{convicted}'),
+                               'TE_S' = expression('ATE'['M=1'] %*% ' #{convicted}'),
+                               'TE_ST' = expression('ATT'['M=1'] %*% ' #{convicted minorities}')
+                    )
+  ) +
+  scale_linetype_manual(
+    name = 'x',
+    values = c('naive' = NA,
+               'TE_S' = NA,
+               'TE_ST' = 'dashed'
+    ),
+    labels = c('naive' = expression('naïve ATE'['M=1'] %*% ' #{convicted}'),
+               'TE_S' = expression('ATE'['M=1'] %*% ' #{convicted}'),
+               'TE_ST' = expression('ATT'['M=1'] %*% ' #{convicted minorities}')
+    )
+  ) +
+  ylab('Civilians subject to any racially\ndiscriminatory sentence (thousands)\n') +
+  xlab('\nProportion of racially discriminatory convictions') +
+  xlim(0, 1) +
+  facet_grid(mod.label ~ race.label) +
+  theme(text = element_text(size = 12))
+ggsave('plots_full_3rdQ.pdf', width = 10, height = 9)
+
+# thresh <- 1:3
+# for (mod.type in c('base', 'full')){
+#   for (treat.race in c('black')){
+#     keep <-
+#       results$thresh %in% thresh &
+#       results$mod.type == mod.type &
+#       results$treat.race == treat.race &
+#       results$rho <= .8
+#     p <- ggplot(results[keep & results$qoi == 'ates',],
+#                 aes(x = rho)
+#     ) +
+#       geom_hline(yintercept = 0,
+#       ) +
+#       geom_ribbon(aes(ymin = min.cilo * nrow(df) / 1000,
+#                       ymax = max.cihi * nrow(df) / 1000
+#       ),
+#       fill = paste0(red, '80')
+#       ) +
+#       geom_ribbon(aes(ymin = min * nrow(df) / 1000,
+#                       ymax = max * nrow(df) / 1000,
+#                       fill = 'TE_S'
+#       )
+#       ) +
+#       geom_line(aes(y = est * n.minority / 1000,
+#                     linetype = 'TE_ST',
+#       ),
+#       results[keep & results$qoi == 'atest',
+#       ],
+#       color = 'black',
+#       size = 1
+#       ) +
+#       geom_point(aes(y = est * nrow(df) / 1000,
+#                      color = 'naive'
+#       ),
+#       data = results[keep & results$rho == 0,],
+#       color = blue,
+#       size = 2
+#       ) +
+#       geom_errorbar(aes(ymin = est.cilo * nrow(df) / 1000,
+#                         ymax = est.cihi * nrow(df) / 1000,
+#                         color = 'naive'
+#       ),
+#       data = results[keep & results$rho == 0,],
+#       width = 1,
+#       size = 1
+#       ) +
+#       geom_vline(aes(xintercept = rho),
+#                  data = annotations[annotations$treat.race == treat.race,],
+#                  size = .25
+#       ) +
+#       ## geom_text(aes(label = text),
+#       ##           data = annotations[annotations$treat.race == treat.race,],
+#       ##           y = 1550,
+#       ##           size = 5,
+#       ##           hjust = 1,
+#       ##           vjust = -.5,
+#       ##           angle = 90
+#       ##           ) +
+#       theme_light(base_size = 24) +
+#       theme(legend.position = 'bottom',
+#             legend.key.size = unit(1, 'cm'),
+#             legend.spacing.x = unit(.25, 'cm'),
+#             panel.grid.minor = element_blank(),
+#             panel.grid.major = element_line(linetype = 'dashed')
+#       ) +
+#       guides(color = guide_legend(title = NULL, order = 1),
+#              linetype = guide_legend(title = NULL, order = 2),
+#              fill = guide_legend(title = NULL, order = 3)
+#       ) +
+#       scale_color_manual(
+#         name = 'x',
+#         values = c('naive' = blue),
+#         labels = expression('naïve ATE'['M=1'] %*% ' #{convicted}')
+#       ) +
+#       scale_fill_manual(name = 'x',
+#                         values = c('TE_S' = red),
+#                         labels = expression('ATE'['M=1'] %*% ' #{convicted}')
+#       ) +
+#       scale_linetype_manual(
+#         name = 'x',
+#         values = c('TE_ST' = 'dashed'),
+#         labels = expression('ATT'['M=1'] %*% ' #{convicted minorities}')
+#       ) +
+#       ylab('Civilians subject to any racially\ndiscriminatory sentence (thousands)\n') +
+#       xlab('\nProportion of discriminatory convictions') +
+#       xlim(0, .8) +
+#       facet_wrap('thresh.label.short', scales = 'free') +
+#       theme(legend.text = element_text(size = 17))
+#     # ptitle <- sprintf('pics/bounds_other_correct_%s_%s.pdf', mod.type, treat.race)
+#     # ggsave(ptitle, plot = p, width = 14, height = 8)
+#   }
+#   
+# }
+# 
+# 
+# 
